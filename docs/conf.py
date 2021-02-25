77357922921d0531dca7dc1818227b0e8e7538e3
Boyana Norris <norris@cs.uoregon.edu>
2021-02-25 07:52:22 -0800

    authors

--- docs/conf.py
+++ docs/conf.py
@@ -55,8 +55,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Python Performance Analysis Recipes'
-copyright = '2021, Boyana Norris'
-author = 'Boyana Norris'
+copyright = '2021, Brian Gravelle & Boyana Norris'
+author = 'Brian Gravelle and Boyana Norris'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
