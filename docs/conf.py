ef0e3f24a199044be8badd924dc1edb62997498c
Boyana Norris <norris@cs.uoregon.edu>
2021-02-25 08:03:11 -0800

    docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 
 import sys
 import os
-import sphinx_rtd_theme
+import astropy_sphinx_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -115,10 +114,17 @@ todo_include_todos = True
 # -- Options for HTML output ----------------------------------------------
 
 
-html_theme = "sphinx_rtd_theme"
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+html_theme_path = astropy_sphinx_theme.get_html_theme_path()
+html_theme = 'bootstrap-astropy'
+html_theme_options = {
+    'logotext1': 'packagename',  # white,  semi-bold
+    'logotext2': '',  # orange, light
+    'logotext3': ':docs',   # white,  light
+    'astropy_project_menubar': True
+    }
+
 #html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
